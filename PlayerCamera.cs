e6754ebabb7f4650696e6abee7a88d395981a6a7
Objelisks <supertyrian@gmail.com>
2016-01-29 14:00:59 -0800

    basic stuff, simple models

---
+++ PlayerCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public Transform player;
+	public Vector3 targetPosition;
+	public bool useTargetPosition = false;
+	public float cameraSpeed = 1.0f;
+
+	public Vector3 offset = new Vector3 (-5, 10, -5);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 target;
+		if (useTargetPosition) {
+			target = targetPosition;
+		} else {
+			target = player.position + offset;
+		}
+		var nextPosition = Vector3.Slerp (transform.position, target, Time.smoothDeltaTime * cameraSpeed);
+		transform.position = nextPosition;
+	}
+}
---
+++ Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float moveSpeed = 1.0f;
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		var moveInput = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		var camera = Camera.main.transform;
+
+		var rotatedMove = Quaternion.Euler (0, camera.rotation.eulerAngles.y, 0) * moveInput;
+
+		body.MovePosition(transform.position + rotatedMove * Time.smoothDeltaTime * moveSpeed);
+	}
+}
